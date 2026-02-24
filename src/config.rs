e88b148cb7f3a690bcb27e0b2746de5ea7e94466
share121 <me@s121.top>
2026-02-24 23:56:53 +0800

    chore: 为 Config 结构体添加 Default 实现

--- src/config.rs
+++ src/config.rs
@@ -31,6 +31,31 @@ pub struct Config {
     pub local_address: Vec<IpAddr>,
     pub max_speculative: usize,
     pub write_method: WriteMethod,
+    /// 已经下载过的部分
     pub have_been_downloaded_chunk: Vec<ProgressEntry>,
+    /// 滤除小于 chunk_window 的部分
     pub chunk_window: u64,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            save_dir: PathBuf::new(),
+            threads: 32,
+            proxy: Proxy::System,
+            headers: HashMap::new(),
+            min_chunk_size: 8 * 1024 * 1024,
+            write_buffer_size: 16 * 1024 * 1024,
+            write_queue_cap: 10240,
+            retry_gap: Duration::from_millis(500),
+            pull_timeout: Duration::from_secs(5),
+            accept_invalid_certs: false,
+            accept_invalid_hostnames: false,
+            local_address: Vec::new(),
+            max_speculative: 3,
+            write_method: WriteMethod::Mmap,
+            have_been_downloaded_chunk: Vec::new(),
+            chunk_window: 8 * 1024,
+        }
+    }
 }
