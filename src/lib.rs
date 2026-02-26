1c90e5b73e1d688a2cb77b7ade21990a6509d354
share121 <me@s121.top>
2026-02-26 18:43:13 +0800

    chore: 导出fast_down中常用函数

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,9 @@ pub use download::*;
 pub use error::*;
 pub use event::*;
 pub use fast_down;
+pub use fast_down::{
+    AnyError, BoxPusher, DownloadResult, Event as RawEvent, FileId, InvertIter, Merge,
+    ProgressEntry, PullResult, PullStream, Puller, PullerError, Pusher, Total, UrlInfo, WorkerId,
+    file, handle, http, invert, mem, mock, multi, reqwest as reqwest_adapter, single, utils,
+};
 pub use reqwest;
